6fe51cfccfe2c6b46bc62c6e71b2d237160d59bb
YomachiN <yomachi.nwosu@gmail.com>
2025-05-05 17:25:20 +0100

    Final Commit
    Final submission

--- script.js
+++ script.js
@@ -9,8 +9,11 @@ document.addEventListener('DOMContentLoaded', function () {
       const name = document.getElementById('name').value.trim();
       const email = document.getElementById('email').value.trim();
       const message = document.getElementById('message').value.trim();
+      const age = document.getElementById('age').value.trim();
+      
+  handleformsubmission(name, email, message, age);
   
- 
+      
       if (name === '' || email === '' || message === '') {
         responseDiv.innerHTML = '<p style="color:red;">Please fill out all fields.</p>';
         return;
@@ -20,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
         <h3>Thank you for contacting me, ${name}!</h3>
         <p><strong>Email:</strong> ${email}</p>
         <p><strong>Message:</strong> ${message}</p>
+        <p><strong>Age:</strong> ${age}</p>
       `;
 
   form.reset();
